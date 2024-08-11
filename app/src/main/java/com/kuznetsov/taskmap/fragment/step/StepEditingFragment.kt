af15d0d2ee89afea399346c3d60d5f97e62d3dd2
Олег <oleg.kyznetzov@mail.ru>
2024-08-11 18:33:11 +0400

    updated step editing

--- app/src/main/java/com/kuznetsov/taskmap/fragment/step/StepEditingFragment.kt
+++ app/src/main/java/com/kuznetsov/taskmap/fragment/step/StepEditingFragment.kt
@@ -1,18 +1,22 @@
 package com.kuznetsov.taskmap.fragment.step
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.kuznetsov.taskmap.R
 import com.kuznetsov.taskmap.dao.GoalDatabase
 import com.kuznetsov.taskmap.databinding.FragmentStepEditingBinding
 import com.kuznetsov.taskmap.viewmodel.step.StepEditingViewModel
 import com.kuznetsov.taskmap.viewmodel.step.StepEditingViewModelFactory
 
-
+const val TAG = "StepEditingFragment"
 class StepEditingFragment : Fragment() {
     private var _binding: FragmentStepEditingBinding? = null
     private val binding get() = _binding!!
@@ -35,6 +39,26 @@ class StepEditingFragment : Fragment() {
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+
+        viewModel.isNavigatedToStepShowingFragment.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                viewModel.step.value?.let {
+                    val action = StepEditingFragmentDirections
+                        .actionStepEditingFragmentToStepFragment(it.subGoalId)
+                    findNavController().navigate(action)
+                }
+                viewModel.afterNavigateToStepShowingFragment()
+            }
+        })
+
+
+        viewModel.isMadeToast.observe(viewLifecycleOwner, Observer {
+            Log.d(TAG, "isMadeToast, it = $it")
+            if (it) {
+                Toast.makeText(context, viewModel.ERROR_UPDATING, Toast.LENGTH_SHORT).show()
+                viewModel.afterMadeToast()
+            }
+        })
 
         return view
     }
--- app/src/main/java/com/kuznetsov/taskmap/viewmodel/step/StepEditingViewModel.kt
+++ app/src/main/java/com/kuznetsov/taskmap/viewmodel/step/StepEditingViewModel.kt
@@ -1,15 +1,80 @@
 package com.kuznetsov.taskmap.viewmodel.step
 
+import android.support.v4.os.IResultReceiver._Parcel
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kuznetsov.taskmap.dao.StepDao
+import kotlinx.coroutines.launch
+
 
 class StepEditingViewModel(val stepDao: StepDao, val stepId: Long): ViewModel() {
+    val ERROR_UPDATING = "Step hasn't been updated!"
 
     val step = stepDao.get(stepId)
+    var newName = MutableLiveData("")
+    var newStartResult = MutableLiveData("")
+    var newCurrentResult = MutableLiveData("")
+    var newFinishResult = MutableLiveData("")
 
-    var newName = step.value?.name ?: "Error: not found"
-    var newStartResult = step.value?.startResult.toString()
-    var newCurrentResult = step.value?.currentResult.toString()
-    var newFinishResult = step.value?.finishResult.toString()
+    private var _isNavigatedToStepShowingFragment = MutableLiveData<Boolean>()
+    val isNavigatedToStepShowingFragment: LiveData<Boolean> get()
+        = _isNavigatedToStepShowingFragment
 
+    private var _isMadeToast = MutableLiveData<Boolean>()
+    val isMadeToast: LiveData<Boolean> get() = _isMadeToast
+    init {
+        _isNavigatedToStepShowingFragment.value = false
+        _isMadeToast.value = false
+    }
+    fun navigateToStepShowingFragment() {
+        _isNavigatedToStepShowingFragment.value = true
+    }
+
+    fun afterNavigateToStepShowingFragment() {
+        _isNavigatedToStepShowingFragment.value = false
+    }
+
+    fun afterMadeToast() {
+        _isMadeToast.value = false
+    }
+
+    fun cleanFields() {
+        newName.value = ""
+        newStartResult.value = ""
+        newCurrentResult.value = ""
+        newFinishResult.value = ""
+    }
+
+    fun delete() {
+        step.value?.let {
+            viewModelScope.launch {
+                stepDao.delete(it)
+            }
+            navigateToStepShowingFragment()
+        }
+    }
+
+    fun update() {
+        val name = newName.value ?: ""
+        val start = newStartResult.value?.toLongOrNull() ?: -1
+        val current = newCurrentResult.value?.toLongOrNull() ?: -1
+        val finish = newFinishResult.value?.toLongOrNull() ?: -1
+        if (start < 0 || current < 0 || finish < 0 || name.length == 0) {
+            _isMadeToast.value = true
+        } else {
+            step.value?.let {
+                it.name = name
+                it.startResult = start
+                it.currentResult = current
+                it.finishResult = finish
+                viewModelScope.launch {
+                    stepDao.update(it)
+                }
+                cleanFields()
+            }
+        }
+    }
 }
